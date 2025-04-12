8254881f55167ea0e81ae7064cb93be4242ea217
Zoe Ben Bogner <153617265+zoebbogner@users.noreply.github.com>
2025-04-12 21:52:55 +0300

    Create Showtime.java

---
+++ src/main/java/com/att/tdp/popcorn_palace/model/Showtime.java
@@ -0,0 +1,35 @@
+package com.att.tdp.popcorn_palace.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "showtimes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Showtime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @Column(nullable = false)
+    private String theater;
+
+    @Column(name = "start_time", nullable = false)
+    private ZonedDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private ZonedDateTime endTime;
+
+    @Column(nullable = false)
+    private double price;
+} 
