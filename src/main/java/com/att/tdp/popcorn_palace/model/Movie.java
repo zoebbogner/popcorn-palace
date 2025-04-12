19c01505d37cc12afbd33cdc2bf2e0c04f1af8b0
Zoe Ben Bogner <153617265+zoebbogner@users.noreply.github.com>
2025-04-12 18:25:30 +0300

    Added movie entity

---
+++ src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
@@ -0,0 +1,27 @@
+package com.att.tdp.popcorn_palace.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String genre;
+    private int duration;
+    private double rating;
+    private int releaseYear;
+} 
