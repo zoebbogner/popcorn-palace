c6fe090d9a0cf08fb53e1e25638f1fb5188351c1
Zoe Ben Bogner <153617265+zoebbogner@users.noreply.github.com>
2025-04-12 21:45:41 +0300

    Update MovieControllerTest.java

--- src/test/java/com/att/tdp/popcorn_palace/controller/MovieControllerTest.java
+++ src/test/java/com/att/tdp/popcorn_palace/controller/MovieControllerTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
