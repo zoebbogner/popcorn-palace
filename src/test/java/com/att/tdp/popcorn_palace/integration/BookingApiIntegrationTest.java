438b1c2309a0dd17257a47c165c038e74bbb8f2a
Zoe Ben Bogner <153617265+zoebbogner@users.noreply.github.com>
2025-04-15 19:29:05 +0200

    Update BookingApiIntegrationTest.java

--- src/test/java/com/att/tdp/popcorn_palace/integration/BookingApiIntegrationTest.java
+++ src/test/java/com/att/tdp/popcorn_palace/integration/BookingApiIntegrationTest.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
 
-import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
